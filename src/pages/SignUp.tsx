1fdd5876afa7802a48c8df6731f342b15871e8fa
Anil Kumar <114756436+anil-52026@users.noreply.github.com>
2024-08-15 02:02:01 +0530

    Update SignUp.tsx

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from "react-router-dom"; 
 
 function SignUp() {
-  const Navigate = useNavigate()
   const [firstName, setFirstName] = useState<string>('');
   const [lastName, setLastName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
